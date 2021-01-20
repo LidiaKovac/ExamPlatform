d6544838a55b9aef4cd42bbfea8036c9149bba0f
Lidia Kovac <lidia.kovac1998@gmail.com>
2021-01-20 11:57:38 +0100

    Revert "backup push"
    This reverts commit cb76806ac7a2361c106f712144acf0a7b3aa7292.

--- src/components/Exam.jsx
+++ src/components/Exam.jsx
@@ -5,10 +5,7 @@ import "../styling/Exam.scss";
 class Exam extends React.Component {
   state = {
     exam: {},
-    answerBody: {
-      "question": 0,
-      "answer": 0,
-    },
+    questNum: 0,
     questions: [],
   };
   componentDidMount = async () => {
@@ -17,90 +14,61 @@ class Exam extends React.Component {
     );
     const exam = await examDB.json();
     this.setState({ exam: exam[0] });
-    this.setState({ questions: this.state.exam.questions });
+    this.setState({ questions: this.state.exam.questions })
   };
-  nextQuestion = async () => {
-    if (this.state.answerBody.question < 4) {
-      this.setState(
-        {
-          answerBody: {
-            ...this.state.answerBody,
-            "question": this.state.answerBody.question + 1,
-          },
-        }
-      );
-    } else if (this.state.answerBody.question === 4) {
-      console.log("Done!");
+  nextQuestion = async() => {
+    console.log(this.state.exam)
+      if (this.state.questNum < 4) {
+      this.setState({questNum: this.state.questNum+1})}
+      else if(this.state.questNum === 4) {
+        console.log("Done!")
+
     }
-    try {
-      const question = await this.state.answerBody.question
-      const answer = await this.state.answerBody.answer
-      console.log(question, answer)
-      const response = await fetch(
-        "http://localhost:3001/exam/" + this.props.id._id + "/answer",
-        {
-          method: "POST",
-          body: JSON.stringify(this.state.answerBody),
-        }
-      );
-      let exam = await response.json();
-      (await this.state.exam.score) < 5
-        ? this.setState({ exam: exam[0] }, () =>
-            console.log("score: ", this.state.exam.score)
-          )
-        : console.log("You already reached the max points for this exam. ");
-    } catch (error) {
-      console.log(error);
+    try{
+      const response = await fetch("http://localhost:3001/exam/" + this.props.id._id + "/answer", {
+      method: 'POST',
+      body: {
+        question: this.state.questNum,
+        answer: this.state.answer
+      }
+    })
+    let exam = await response.json()
+    await this.state.exam.score < 5 ? 
+      this.setState({exam: exam.exam[0]}, ()=>console.log("score: ", this.state.exam.score)) : 
+      console.log("You already reached the max points for this exam. ")
+  }catch(error) {
+      console.log(error)
     }
-  };
+  }
 
   handleAnswer = (e) => {
-    console.log(e.target.id);
-    this.setState({
-      answerBody: {
-
-        ...this.state.answerBody,
-        "answer": parseInt(e.target.id),
-      },
-    });
-  };
+    console.log(e.target.id)
+    this.setState({answer: e.target.id})
+  }
 
   render() {
     return (
       <div className="wrap">
-        {this.state.answerBody.question < 4 ? (
-          <div className="exam-body-start">
-            <div className="question-text">
-              {this.state.questions[this.state.answerBody.question] &&
-                this.state.questions[this.state.answerBody.question].text}
-            </div>
-            <div className="answer-wrap">
-              {this.state.questions[this.state.answerBody.question] &&
-                this.state.questions[
-                  this.state.answerBody.question
-                ].answers.map((answ, index) => (
+        {this.state.questNum < 4 ? (<div className="exam-body-start">
+          <div className="question-text">
+            {this.state.questions[this.state.questNum] &&
+              this.state.questions[this.state.questNum].text}
+          </div>
+          <div className="answer-wrap">
+            {this.state.questions[this.state.questNum] &&
+              this.state.questions[this.state.questNum].answers.map(
+                (answ, index) => (
                   <div className="answer-body">
-                    <input
-                      type="radio"
-                      id={index}
-                      name="answer"
-                      onChange={(e) => this.handleAnswer(e)}
-                    />
+                    <input type="radio" id={index} name="answer" onChange={(e)=>this.handleAnswer(e)} />
                     <label for={index}>{answ.text}</label>
                   </div>
-                ))}
-            </div>
+                )
+              )}
           </div>
-        ) : (
-          <div className="exam-body-start">
-            The exam is done! Here is your score: {this.state.exam.score}
-          </div>
-        )}
-        {this.state.answerBody.question < 4 && (
-          <div className="next-btn rounded-pill" onClick={this.nextQuestion}>
+        </div>) : <div className='exam-body-start'>The exam is done! Here is your score: {this.state.exam.score}</div> }
+        {this.state.questNum < 4 && <div className='next-btn rounded-pill' onClick={this.nextQuestion}>
             SUBMIT
-          </div>
-        )}
+        </div>}
       </div>
     );
   }
